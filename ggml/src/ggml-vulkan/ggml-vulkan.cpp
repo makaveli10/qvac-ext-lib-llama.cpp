865ed7d73b56c6096f0156edb90c028c8d3e0fd8
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2025-08-29 10:45:30 +0200

    vulkan: Point out lack of storageBuffer8BitAccess.
    Several shaders use storage buffers with uint8_t types.
    Log a warning in case this is not supported by the device,
    e.g. Adreno 650.
    
    For proper backwards compatibility an ifdef workaround
    needs to be implemented for falling back to 32-bit wide
    storage block variables in this case.
    
    See: https://docs.vulkan.org/guide/latest/extensions/shader_features.html#VK_KHR_16bit_storage
    
    Impacted shaders (not limited to):
    ```
    mul_mat_vec_q8_0_f32_f32_1
    mul_mat_vec_q8_0_f32_f32_2
    matmul_q8_0_f32_f16acc_aligned_s
    ```

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -3949,6 +3949,10 @@ static vk_device ggml_vk_get_device(size_t idx) {
 
         vkGetPhysicalDeviceFeatures2(device->physical_device, &device_features2);
 
+        if (vk12_features.storageBuffer8BitAccess == VK_FALSE) {
+            GGML_LOG_WARN("Vulkan device does not support storageBuffer8BitAccess.\n");
+        }
+
         device->fp16 = device->fp16 && vk12_features.shaderFloat16;
 
 #if defined(VK_KHR_shader_bfloat16)
