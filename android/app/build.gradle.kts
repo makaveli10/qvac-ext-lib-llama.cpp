e08038a3ae3a417d9280fb15450837726e442c92
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2025-09-04 11:09:14 +0200

    android: Add native Android build.
    Based on:
    * Android Studio C++ template.
    * Native Activity NDK example.
    
    * Build full project.
    * Enable Vulkan.
    * Enable build of tools and tests.
    * Enable JNI activity.
    * Build examples.
    * Build common module.
    * Build as release by default.
    
    * Call main build as submodule from Android.
    
    * Copy native and Kotlin code from examples.
    Rename llama-android.cpp to llama-app.cpp.
    Move to ai.ggml.llamacpp namespace.
    LlamaAndroid: Update namespace.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+@Suppress("UnstableApiUsage")
+android {
+    namespace = "ai.ggml.llamacpp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "ai.ggml.llamacpp"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+
+    defaultConfig {
+        externalNativeBuild {
+            cmake {
+                arguments(
+                    "-DGGML_OPENMP=OFF",
+                    "-DGGML_LLAMAFILE=OFF",
+                    "-DLLAMA_CURL=OFF",
+                    "-DGGML_VULKAN=1",
+                    "-DLLAMA_BUILD_TESTS=1", // test need tools
+                    "-DLLAMA_BUILD_TOOLS=1",
+                    "-DLLAMA_BUILD_EXAMPLES=1",
+                    "-DLLAMA_BUILD_COMMON=ON",
+                    "-DCMAKE_BUILD_TYPE=Release"
+                )
+
+                abiFilters("arm64-v8a")
+            }
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
