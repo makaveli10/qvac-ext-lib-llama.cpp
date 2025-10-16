364589a4312135a39929647963c0a4e02f91abb8
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2025-10-16 17:18:45 +0200

    android/app: Improve alignment.

--- android/app/src/main/java/ai/ggml/llamacpp/MainActivity.kt
+++ android/app/src/main/java/ai/ggml/llamacpp/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,6 +32,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -140,13 +142,20 @@ fun MainCompose(
                 }
             }
         }
-        Row {
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             OutlinedTextField(
                 value = viewModel.message,
                 onValueChange = { viewModel.updateMessage(it) },
-                label = { Text("Message") },
+                label = { Text("Prompt") },
+                modifier = Modifier.weight(1f),
+                singleLine = true,
             )
-            Button(onClick = { viewModel.send() }) {
+            Button(
+                onClick = { viewModel.send() },
+            ) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.Send,
                     contentDescription = "Send",
