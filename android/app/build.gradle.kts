3d4870e1d973d5b3e481dc516b4e4e3061a305c3
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2025-10-16 14:04:26 +0200

    android/build: Enable compose.
    * Add compose deps.
    * Add Material 3 deps.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 @Suppress("UnstableApiUsage")
@@ -63,6 +64,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
@@ -71,6 +73,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.ui)
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
