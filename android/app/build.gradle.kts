6d9a9a5f40b74052d6eb2a827d6815a3655ca864
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2025-10-31 13:14:06 +0100

    android/build: Bump minsdk to 31

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "ai.ggml.llamacpp"
-        minSdk = 29
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
